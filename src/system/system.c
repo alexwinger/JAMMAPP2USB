fb2a1815041328951f6b68796acbe6df645aeecd
Alejandro Lopez <alopezh@dextratech.com>
2017-05-18 12:37:49 -0500

    First commit with all code    - LEDS and Buttons Code Removed

---
+++ src/system/system.c
@@ -0,0 +1,301 @@
+/*******************************************************************************
+Copyright 2016 Microchip Technology Inc. (www.microchip.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+To request to license the code under the MLA license (www.microchip.com/mla_license), 
+please contact mla_licensing@microchip.com
+*******************************************************************************/
+
+#include "system.h"
+
+// PIC18F2553 Configuration Bit Settings
+// 'C' source line config statements
+// CONFIG1L
+#pragma config PLLDIV = 1       // PLL Prescaler Selection bits (No prescale (4 MHz oscillator input drives PLL directly))
+#pragma config CPUDIV = OSC1_PLL2// System Clock Postscaler Selection bits ([Primary Oscillator Src: /1][96 MHz PLL Src: /2])
+#pragma config USBDIV = 2       // USB Clock Selection bit (used in Full-Speed USB mode only; UCFG:FSEN = 1) (USB clock source comes from the 96 MHz PLL divided by 2)
+
+// CONFIG1H
+#pragma config FOSC = HSPLL_HS  // Oscillator Selection bits (HS oscillator, PLL enabled (HSPLL))
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+// CONFIG2L
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOR = ON         // Brown-out Reset Enable bits (Brown-out Reset enabled in hardware only (SBOREN is disabled))
+#pragma config BORV = 3         // Brown-out Reset Voltage bits (Minimum setting)
+#pragma config VREGEN = OFF     // USB Voltage Regulator Enable bit (USB voltage regulator disabled)
+
+// CONFIG2H
+#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+
+// CONFIG3H
+#pragma config CCP2MX = ON      // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = OFF     // PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
+#pragma config LPT1OSC = ON     // Low-Power Timer 1 Oscillator Enable bit (Timer1 configured for low-power operation)
+#pragma config MCLRE = OFF      // MCLR Pin Enable bit (RE3 input pin enabled; MCLR pin disabled)
+
+// CONFIG4L
+#pragma config STVREN = OFF     // Stack Full/Underflow Reset Enable bit (Stack full/underflow will not cause Reset)
+#pragma config LVP = ON         // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
+#ifndef __XC8
+#pragma config XINST = ON       // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode enabled)
+#endif
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000800-001FFFh) is not code-protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (002000-003FFFh) is not code-protected)
+#pragma config CP2 = OFF        // Code Protection bit (Block 2 (004000-005FFFh) is not code-protected)
+#pragma config CP3 = OFF        // Code Protection bit (Block 3 (006000-007FFFh) is not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) is not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM is not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000800-001FFFh) is not write-protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (002000-003FFFh) is not write-protected)
+#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (004000-005FFFh) is not write-protected)
+#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (006000-007FFFh) is not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) are not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot block (000000-0007FFh) is not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM is not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000800-001FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (002000-003FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (004000-005FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (006000-007FFFh) is not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) is not protected from table reads executed in other blocks)
+
+typedef enum
+{
+    SOFT_START_POWER_OFF,
+    SOFT_START_POWER_START,
+    SOFT_START_POWER_ENABLED,
+    SOFT_START_POWER_READY
+} SOFT_START_STATUS;
+
+static SOFT_START_STATUS softStartStatus = SOFT_START_POWER_OFF;
+
+/*********************************************************************
+* Function: void SYSTEM_Initialize( SYSTEM_STATE state )
+*
+* Overview: Initializes the system.
+*
+* PreCondition: None
+*
+* Input:  SYSTEM_STATE - the state to initialize the system into
+*
+* Output: None
+*
+********************************************************************/
+void SYSTEM_Initialize( SYSTEM_STATE state )
+{
+    switch(state)
+    {
+        case SYSTEM_STATE_USB_START:
+            //On the PIC18F46J50 Family of USB microcontrollers, the PLL will not power up and be enabled
+            //by default, even if a PLL enabled oscillator configuration is selected (such as HS+PLL).
+            //This allows the device to power up at a lower initial operating frequency, which can be
+            //advantageous when powered from a source which is not gauranteed to be adequate for 48MHz
+            //operation.  On these devices, user firmware needs to manually set the OSCTUNE<PLLEN> bit to
+            //power up the PLL.
+            {
+                unsigned int pll_startup_counter = 600;
+//                OSCTUNEbits.PLLEN = 1;  //Enable the PLL and wait 2+ms until the PLL locks before enabling USB module
+                while(pll_startup_counter--);
+            }
+            //Device switches over automatically to PLL output after PLL is locked and ready.
+
+            
+            BUTTON_Enable(BUTTON_USB_DEVICE_HID_JOYSTICK);
+			
+            softStartStatus = SOFT_START_POWER_START;
+            break;
+
+        case SYSTEM_STATE_USB_SUSPEND: 
+            //Configure device for low power consumption.  Turn off app power.
+            AppPowerDisable();
+            softStartStatus = SOFT_START_POWER_OFF;
+            break;
+            
+        case SYSTEM_STATE_USB_RESUME:
+            softStartStatus = SOFT_START_POWER_START;
+            break;
+    }
+}
+
+/*********************************************************************
+* Function: void SYSTEM_Tasks(void)
+*
+* Overview: Runs system level tasks that keep the system running
+*
+* PreCondition: System has been initalized with SYSTEM_Initialize()
+*
+* Input: None
+*
+* Output: None
+*
+********************************************************************/
+void SYSTEM_Tasks(void)
+{
+    switch(softStartStatus)
+    {
+        case SOFT_START_POWER_OFF:
+            break;
+
+        case SOFT_START_POWER_START:
+            if(USBGetDeviceState() != CONFIGURED_STATE)
+            {
+                break;
+            }
+            
+            AppPowerEnable();
+            softStartStatus = SOFT_START_POWER_ENABLED;
+            break;
+
+        case SOFT_START_POWER_ENABLED:
+            if(AppPowerReady() == true)
+            {
+                softStartStatus = SOFT_START_POWER_READY;
+//                LED_Enable(LED_USB_DEVICE_STATE);
+            }
+            break;
+            
+        case SOFT_START_POWER_READY:
+            break;
+    }
+}
+
+			
+			
+#if defined(__XC8)
+void interrupt SYS_InterruptHigh(void)
+{
+    #if defined(USB_INTERRUPT)
+        USBDeviceTasks();
+    #endif
+}
+#else
+        void YourHighPriorityISRCode();
+        void YourLowPriorityISRCode();
+        
+	//On PIC18 devices, addresses 0x00, 0x08, and 0x18 are used for
+	//the reset, high priority interrupt, and low priority interrupt
+	//vectors.  However, the current Microchip USB bootloader
+	//examples are intended to occupy addresses 0x00-0x7FF or
+	//0x00-0xFFF depending on which bootloader is used.  Therefore,
+	//the bootloader code remaps these vectors to new locations
+	//as indicated below.  This remapping is only necessary if you
+	//wish to program the hex file generated from this project with
+	//the USB bootloader.  If no bootloader is used, edit the
+	//usb_config.h file and comment out the following defines:
+	//#define PROGRAMMABLE_WITH_USB_HID_BOOTLOADER
+	//#define PROGRAMMABLE_WITH_USB_LEGACY_CUSTOM_CLASS_BOOTLOADER
+
+	#if defined(PROGRAMMABLE_WITH_USB_HID_BOOTLOADER)
+		#define REMAPPED_RESET_VECTOR_ADDRESS			0x1000
+		#define REMAPPED_HIGH_INTERRUPT_VECTOR_ADDRESS	0x1008
+		#define REMAPPED_LOW_INTERRUPT_VECTOR_ADDRESS	0x1018
+	#elif defined(PROGRAMMABLE_WITH_USB_MCHPUSB_BOOTLOADER)
+		#define REMAPPED_RESET_VECTOR_ADDRESS			0x800
+		#define REMAPPED_HIGH_INTERRUPT_VECTOR_ADDRESS	0x808
+		#define REMAPPED_LOW_INTERRUPT_VECTOR_ADDRESS	0x818
+	#else
+		#define REMAPPED_RESET_VECTOR_ADDRESS			0x00
+		#define REMAPPED_HIGH_INTERRUPT_VECTOR_ADDRESS	0x08
+		#define REMAPPED_LOW_INTERRUPT_VECTOR_ADDRESS	0x18
+	#endif
+
+	#if defined(PROGRAMMABLE_WITH_USB_HID_BOOTLOADER)||defined(PROGRAMMABLE_WITH_USB_MCHPUSB_BOOTLOADER)
+	extern void _startup (void);        // See c018i.c in your C18 compiler dir
+	#pragma code REMAPPED_RESET_VECTOR = REMAPPED_RESET_VECTOR_ADDRESS
+	void _reset (void)
+	{
+	    _asm goto _startup _endasm
+	}
+	#endif
+
+        #pragma code REMAPPED_HIGH_INTERRUPT_VECTOR = REMAPPED_HIGH_INTERRUPT_VECTOR_ADDRESS
+	void Remapped_High_ISR (void)
+	{
+	     _asm goto YourHighPriorityISRCode _endasm
+	}
+	#pragma code REMAPPED_LOW_INTERRUPT_VECTOR = REMAPPED_LOW_INTERRUPT_VECTOR_ADDRESS
+	void Remapped_Low_ISR (void)
+	{
+	     _asm goto YourLowPriorityISRCode _endasm
+	}
+
+	#if defined(PROGRAMMABLE_WITH_USB_HID_BOOTLOADER)||defined(PROGRAMMABLE_WITH_USB_MCHPUSB_BOOTLOADER)
+	//Note: If this project is built while one of the bootloaders has
+	//been defined, but then the output hex file is not programmed with
+	//the bootloader, addresses 0x08 and 0x18 would end up programmed with 0xFFFF.
+	//As a result, if an actual interrupt was enabled and occured, the PC would jump
+	//to 0x08 (or 0x18) and would begin executing "0xFFFF" (unprogrammed space).  This
+	//executes as nop instructions, but the PC would eventually reach the REMAPPED_RESET_VECTOR_ADDRESS
+	//(0x1000 or 0x800, depending upon bootloader), and would execute the "goto _startup".  This
+	//would effective reset the application.
+
+	//To fix this situation, we should always deliberately place a
+	//"goto REMAPPED_HIGH_INTERRUPT_VECTOR_ADDRESS" at address 0x08, and a
+	//"goto REMAPPED_LOW_INTERRUPT_VECTOR_ADDRESS" at address 0x18.  When the output
+	//hex file of this project is programmed with the bootloader, these sections do not
+	//get bootloaded (as they overlap the bootloader space).  If the output hex file is not
+	//programmed using the bootloader, then the below goto instructions do get programmed,
+	//and the hex file still works like normal.  The below section is only required to fix this
+	//scenario.
+	#pragma code HIGH_INTERRUPT_VECTOR = 0x08
+	void High_ISR (void)
+	{
+	     _asm goto REMAPPED_HIGH_INTERRUPT_VECTOR_ADDRESS _endasm
+	}
+	#pragma code LOW_INTERRUPT_VECTOR = 0x18
+	void Low_ISR (void)
+	{
+	     _asm goto REMAPPED_LOW_INTERRUPT_VECTOR_ADDRESS _endasm
+	}
+	#endif	//end of "#if defined(PROGRAMMABLE_WITH_USB_HID_BOOTLOADER)||defined(PROGRAMMABLE_WITH_USB_LEGACY_CUSTOM_CLASS_BOOTLOADER)"
+
+	#pragma code
+
+
+	//These are your actual interrupt handling routines.
+	#pragma interrupt YourHighPriorityISRCode
+	void YourHighPriorityISRCode()
+	{
+		//Check which interrupt flag caused the interrupt.
+		//Service the interrupt
+		//Clear the interrupt flag
+		//Etc.
+        #if defined(USB_INTERRUPT)
+	        USBDeviceTasks();
+        #endif
+
+	}	//This return will be a "retfie fast", since this is in a #pragma interrupt section
+	#pragma interruptlow YourLowPriorityISRCode
+	void YourLowPriorityISRCode()
+	{
+		//Check which interrupt flag caused the interrupt.
+		//Service the interrupt
+		//Clear the interrupt flag
+		//Etc.
+
+	}	//This return will be a "retfie", since this is in a #pragma interruptlow section
+#endif     
